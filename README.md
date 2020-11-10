27352b8a752ed79ffc532f55310575a94ef8be5e
montasir <montasir1400@gmail.com>
2020-11-10 19:52:08 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### What is this?
+
+Renewable: Please ignore. This package does not ready yet.
+
+
+
+### Installation
+
+`npm i renewable --save`
+
+
---
+++ index.js
@@ -0,0 +1,13 @@
+/*********************************************/
+/*  Author : Montasir Mirghani               */
+/*  Email  : contact@montasir.me             */
+/*  GitHub : https://github.com/dr-montasir  */
+/*********************************************/
+
+const useRenewable = () => {
+	return console.log(`Renewable: Please ignore. This package does not ready yet.`);
+};
+
+const renew = useRenewable();
+
+module.exports.renew = renew;
